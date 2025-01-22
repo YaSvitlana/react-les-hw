b2a09d2b7b6c1356e2b794c0161e73f707101f61
YaSvitlana <dubutska14@gmail.com>
2025-01-22 18:42:49 +0200

    hw3-2-help

---
+++ src/components/user-component/UserComponent.tsx
@@ -0,0 +1,21 @@
+import {IUser} from "../../models/IUser.ts";
+import {useNavigate} from "react-router";
+import {FC} from "react";
+
+type Props={
+    user: IUser;
+}
+export const UserComponent:FC<Props> = ({user}) => {
+
+    const navigation = useNavigate()
+    const onButtonClickNavigate = () => {
+            navigation('/users/' + user.id+'/carts')
+        }
+
+            return <div>
+        {user.username}: {user.email}
+
+
+                <button onClick={onButtonClickNavigate}>Click here</button>
+        </div>;
+}
---
+++ src/models/ICart.ts
@@ -0,0 +1,10 @@
+import {IProduct} from "./IProduct.ts"
+export interface ICart {
+	id: number;
+	products: IProduct[];
+	total: number;
+	discountedTotal: number;
+	userId: number;
+	totalProducts: number;
+	totalQuantity: number;
+}
