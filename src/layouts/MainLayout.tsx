81e6145d5f99a07e2d4fde69f5f037fec59204e9
YaSvitlana <dubutska14@gmail.com>
2025-01-24 08:03:57 +0200

    hw3-3

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,7 +1,9 @@
 import {Outlet} from "react-router";
+import {MenuComponent} from "../components/menu/MenuComponent.tsx";
 
 export const MainLayout = () => {
         return <div>
+            <MenuComponent/>
             <Outlet/>
         </div>
     };
